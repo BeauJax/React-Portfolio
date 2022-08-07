1569fe2a65e3d5e2be1a8a993c4fd94b33ba5517
BeauJax <beaujackson88@yahoo.com>
2022-08-07 18:37:15 -0400

    added portfolio data

---
+++ src/data.js
@@ -0,0 +1,21 @@
+export const frontendPortfolio = [
+
+    {
+        id: 1,
+        title: "Find The Dopamine",
+        img: "https://user-images.githubusercontent.com/99527756/169600064-263a002e-667c-4852-8062-0e44a3e50369.png",
+        url: "https://lindseywhite053.github.io/find-the-dopamine/index.html",
+    },
+
+
+]
+
+export const fullstackPortfolio = [
+    {
+        id: 8,
+        title: "The Last Word",
+        img: "https://camo.githubusercontent.com/2d4b9e659e65210fa843399221b33303fa7661a24d01fd3d0ec66b78843907f5/68747470733a2f2f692e696d6775722e636f6d2f394a73645446422e706e67",
+        url: "https://the-last-word-2022.herokuapp.com",
+    },
+   
+]
