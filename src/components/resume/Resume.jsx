8af939bc08e4a74846dab58d0ac65145a7e02299
BeauJax <beaujackson88@yahoo.com>
2022-08-07 19:10:50 -0400

    added resume section

---
+++ src/components/resume/Resume.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./resume.scss"
+
+
+function Resume() {
+
+    return (
+        <div className="resume" id="resume">
+            <h2>Download Beau Jackson Resume</h2>
+            <p>Click here for <a href="https://docs.google.com/document/d/1M3OjVAmmGksSqRd10oYN-fxe5Uzf1YnSXyPR-Br_OIA/edit?usp=sharing" target="_blank" rel="noreferrer">downloadable resume</a></p>
+            <h3>Full Stack Web Development</h3>
+            <p>As a Full Stack Development Coding Bootcamp student, I have developed proficiency with the listed technologies:</p>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>MySQL</li>
+                <li>Sequelize</li>
+                <li>MongoDB</li>
+                <li>Mongoose</li>
+                <li>React</li>
+            </ul>
+        </div>
+    );
+}
